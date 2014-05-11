44a0809bd662ce468456f7e1978b22a839f7c666
thecrazychimpz <glevin@A20909.bcis.bates.edu>
2014-05-11 14:04:28 -0400

    cool

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,4 +1,20 @@
 class PageController < ApplicationController
   def index
   end
+
+  def about
+  end
+
+  def buy
+  end
+
+  def sell
+  end
+
+  def trade
+  end
+
+  def myprofile
+  end
+  
 end
